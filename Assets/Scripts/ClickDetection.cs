274a770e1da233d83781ebca754877567c3ae13c
Carnitter <rottor467@gmail.com>
2018-12-12 15:25:31 +0100

    new click detection
    click to destroy object
    
    range doesnt work correctly yet

---
+++ Assets/Scripts/ClickDetection.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickDetection : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 mousePos = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+            RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero, 100f);
+            if (hit.collider != null)
+            {
+                Destroy(hit.transform.gameObject);
+            }
+
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,19 +12,19 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetMouseButtonDown(0))
-        {
-            destroy = true;
-        }
+    //    if (Input.GetMouseButtonDown(0))
+     //   {
+     //       destroy = true;
+     //   }
     }
 
     private void OnCollisionStay2D(Collision2D collision)
     {
-        if (destroy)
-        {
-            destroy = false;
-            if(collision.gameObject.tag != "Impassable")
-                Destroy(collision.gameObject);
-        }
+      //  if (destroy)
+      //  {
+      //      destroy = false;
+       //     if(collision.gameObject.tag != "Impassable")
+      //          Destroy(collision.gameObject);
+       // }
     }
 }
